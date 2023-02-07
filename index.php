2e2616dc8553e1f20647a623131f56822d49fb10
hernandojr <hernandocostelo@gmail.com>
2023-02-07 19:22:06 +0800

    New added files

---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CDN Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!--External CSS-->
+    <link rel="stylesheet" href="css/index.css">
+    <title>Index Page</title>
+  
+</head>
+  
+<body>
+
+
+<!--Nested Nav Bar -->
+<!-- RESPONSIVE NAV BAR STARTS HERE -->
+<nav class="navbar navbar-expand-sm bg-dark" >
+
+<div class="container-fluid">
+
+  <ul class="navbar-nav">
+
+    <a class="navbar-brand" href="index.php">
+        <img src="pics_logos/atslogo.png" alt="ATS">
+    </a>
+
+<nav class="navbar bg-dark">
+
+    <li class="nav-item">
+        <a class="nav-link" href="#">Add Course</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="#">Add Section</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="#">Add Subject</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="#">Add Subject Timing</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="#">Add Teacher</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="#">Add Rooms</a>
+    </li>
+</nav>   
+    
+
+  </ul>
+
+</div>
+
+</nav>
+    
+  </body>
+</html>
